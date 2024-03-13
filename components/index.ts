18b52e9fa0ad3cd645af2bc33909947e2946af5d
WojciechSz <w.szczygielski00@gmail.com>
2024-03-13 16:17:50 +0100

    first commit

---
+++ components/index.ts
@@ -0,0 +1,23 @@
+import CustomButton from "./CustomButton";
+import Hero from "./Hero";
+import Nav from "./Nav";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+import ShowMore from "./ShowMore";
+
+export {
+  Hero,
+  CustomButton,
+  Nav,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+  CarCard,
+  CarDetails,
+  ShowMore,
+};
